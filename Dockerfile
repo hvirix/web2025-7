51e8c6747b4310db004880e7d1c993a71f745122
hvirix web2025-7 <sofiaasuk3@gmail.com>
2025-05-20 14:08:03 +0300

    фулл ферст парт

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN npm install
 
 COPY . .
 
-CMD ["node", "index.js"]
+CMD npx nodemon index.js --host localhost --port 3000
---
+++ initdb.sql
@@ -0,0 +1,5 @@
+CREATE TABLE devices(
+    id SERIAL PRIMARY KEY,
+    device_name TEXT NOT NULL,
+    serial_number TEXT NOT NULL
+);
